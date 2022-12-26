cb28f9f0ed65e6de59dde2619b74304467e47592
SaniKyn <gajdukevich.sani@mail.ru>
2022-12-26 13:51:41 +0300

    Create_admin_auth_and_todo

---
+++ todowooo/todo/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'from': UserCreationForm()})
+    else:
+
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'from': UserCreationForm(),
+                               'error': 'Такой пользователь существует. Пожалуйста задайте новое значение.'})
+        else:
+            return render(request, 'todo/signupuser.html',
+                          {'from': UserCreationForm(), 'error': 'Пароль не совпадал'})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'from': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',
+                          {'from': AuthenticationForm(), 'error': 'Неверный логин или пароль'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
